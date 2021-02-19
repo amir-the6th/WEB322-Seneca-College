cd1df19fa40b59aa3fe09efd8973c9474c8b7539
asabagh <asabagh@myseneca.ca>
2021-02-19 02:14:48 -0500

    Update data-service.js

--- Assignment-3/web322-app/data-service.js
+++ Assignment-3/web322-app/data-service.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { resolve } = require('path');
 let employees = [];
 let departments = [];
 
@@ -52,4 +53,65 @@ module.exports.getDepartments = function () {
     }
     resolve(departments);
   });
+};
+
+module.exports.addEmployee = function (employeeData) {
+  return new Promise((resolve, reject) => {
+    if (employeeData.isManager == undefined) employeeData.isManager = false;
+    else employeeData.isManager = true;
+    employeeData.employeeNum = employees.length + 1;
+    employees.push(employeeData);
+    resolve(
+      `Employee #${employeeData.employeeNum} has been added successfully!`
+    );
+  });
+};
+
+module.exports.getEmployeesByStatus = function (status) {
+  return new Promise((resolve, reject) => {
+    var empByStatus = [];
+    empByStatus = employees.filter((employee) => {
+      return employee.status == status;
+    });
+    if (empByStatus.length == 0) reject('No Employee by this status found!');
+    else resolve(empByStatus);
+  });
+};
+
+module.exports.getEmployeesByDepartment = function (department) {
+  return new Promise((resolve, reject) => {
+    var empByDepartment = [];
+    empByDepartment = employees.filter((employee) => {
+      return employee.department == department;
+    });
+    if (empByDepartment.length == 0)
+      reject('No Employee by this department found!');
+    else resolve(empByDepartment);
+  });
+};
+
+module.exports.getEmployeesByManager = function (manager) {
+  return new Promise((resolve, reject) => {
+    var empByManager = [];
+    empByManager = employees.filter((employee) => {
+      return employee.employeeManagerNum == manager;
+    });
+    if (empByManager.length == 0)
+      reject('No Employee managed this manager found!');
+    else resolve(empByManager);
+  });
+};
+
+module.exports.getEmployeeByNum = function (num) {
+  return new Promise((resolve, reject) => {
+    var findEmp = employees.filter((employee) => {
+      return employee.employeeNum == num;
+    });
+    if (findEmp.length == 0) reject('No Employee by this id found!');
+    else resolve(findEmp);
+  });
+};
+
+module.exports.getEmployeesByStatus = function (status) {
+  return new Promise((resolve, reject) => {});
 };
