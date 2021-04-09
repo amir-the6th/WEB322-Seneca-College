4eacf6b99501aa318ea401f33cbd3785e7452b86
asabagh <asabagh@myseneca.ca>
2021-04-09 10:55:16 -0400

    Init Assignment 6 from Assignment 5

---
+++ Assignment-6/web322-app/data-service.js
@@ -0,0 +1,451 @@
+const Sequelize = require('sequelize');
+
+// set up sequelize to point to our postgres database
+var sequelize = new Sequelize(
+  'd59ngr6842m6fg',
+  'otnflwyiosqbrq',
+  '918c29fcd072ef5b54664020682caf4f1dc1beebb588a26c1021aebf2de32de8',
+  {
+    host: 'ec2-52-7-115-250.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+  }
+);
+
+// Define our "Employee" and "Department" models
+var Employee = sequelize.define(
+  'Employee',
+  {
+    employeeNum: {
+      type: Sequelize.INTEGER,
+      primaryKey: true, // use "employeeNum" as a primary key
+      autoIncrement: true, // automatically increment the value
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    //maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    hireDate: Sequelize.STRING,
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+  }
+);
+
+var Department = sequelize.define(
+  'Department',
+  {
+    departmentId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    departmentName: Sequelize.STRING,
+  },
+  {
+    createdAt: false,
+    updatedAt: false,
+  }
+);
+
+// Associate Department with Employee
+Department.hasMany(Employee, { foreignKey: 'department' });
+
+module.exports = {
+  initialize: function () {
+    return new Promise((resolve, reject) => {
+      sequelize
+        .sync()
+        .then((Employee) => {
+          resolve();
+        })
+        .then((Department) => {
+          resolve();
+        })
+        .catch((err) => {
+          reject('Unable to sync the database');
+        });
+    });
+  },
+
+  getAllEmployees: function () {
+    return new Promise((resolve, reject) => {
+      Employee.findAll({ raw: true })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else reject('No Employees Found!');
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  getManagers: function () {
+    return new Promise((resolve, reject) => {
+      Employee.findAll({
+        where: {
+          isManager: true,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else {
+            reject('No Employees Found!');
+          }
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  getDepartments: function () {
+    return new Promise((resolve, reject) => {
+      Department.findAll({ raw: true })
+        .then(function (data) {
+          if (data.length > 0) {
+            resolve(data);
+          } else {
+            reject('No Departments Found!');
+          }
+        })
+        .catch(() => reject('No results returned!'));
+    });
+  },
+
+  getEmployeesByStatus: function (e_status) {
+    return new Promise((resolve, reject) => {
+      Employee.findAll({
+        where: {
+          status: e_status,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else reject('No Employees Found!');
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  getEmployeesByDepartment: function (e_department) {
+    return new Promise((resolve, reject) => {
+      Employee.findAll({
+        where: {
+          department: e_department,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else reject('No Employees Found!');
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  getEmployeesByManager: function (manager) {
+    return new Promise((resolve, reject) => {
+      Employee.findAll({
+        where: {
+          employeeManagerNum: manager,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else reject('No Employees Found!');
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  getEmployeeByNum: function (num) {
+    return new Promise((resolve, reject) => {
+      Employee.findAll({
+        where: {
+          employeeNum: num,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else reject('No Employees Found!');
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  getDepartmentById: function (id) {
+    return new Promise((resolve, reject) => {
+      Department.findAll({
+        where: {
+          departmentId: id,
+        },
+        raw: true,
+      })
+        .then((data) => {
+          if (data.length > 0) {
+            resolve(data);
+          } else reject('No Department Found!');
+        })
+        .catch(() => {
+          reject('No results returned!');
+        });
+    });
+  },
+
+  addEmployee: function (employeeData) {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    for (let field in employeeData) {
+      if (employeeData[field] == '') employeeData[field] = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      Employee.create({
+        firstName: employeeData.firstName,
+        lastName: employeeData.lastName,
+        email: employeeData.email,
+        SSN: employeeData.SSN,
+        addressStreet: employeeData.addressStreet,
+        addressCity: employeeData.addressCity,
+        addressState: employeeData.addressState,
+        addressPostal: employeeData.addressPostal,
+        isManager: employeeData.isManager,
+        employeeManagerNum: employeeData.employeeManagerNum,
+        status: employeeData.status,
+        hireDate: employeeData.hireDate,
+        department: employeeData.department,
+      })
+        .then(() => {
+          resolve('New employee added!');
+        })
+        .catch(() => {
+          reject('Unable to add the new employee!');
+        });
+    });
+  },
+
+  updateEmployee: function (employeeData) {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    for (let field in employeeData) {
+      if (employeeData[field] == '') employeeData[field] = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      Employee.update(
+        {
+          firstName: employeeData.firstName,
+          lastName: employeeData.lastName,
+          email: employeeData.email,
+          SSN: employeeData.SSN,
+          addressStreet: employeeData.addressStreet,
+          addressCity: employeeData.addressCity,
+          addressState: employeeData.addressState,
+          addressPostal: employeeData.addressPostal,
+          isManager: employeeData.isManager,
+          employeeManagerNum: employeeData.employeeManagerNum,
+          status: employeeData.status,
+          hireDate: employeeData.hireDate,
+          department: employeeData.department,
+        },
+        {
+          where: {
+            employeeNum: employeeData.employeeNum,
+          },
+        }
+      )
+        .then(() => {
+          resolve('Eemployee information has been updated!');
+        })
+        .catch(() => {
+          reject('Unable to update the employee information!');
+        });
+    });
+  },
+
+  addDepartment: function (departmentData) {
+    for (let field in departmentData) {
+      if (departmentData[field] == '') departmentData[field] = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      Department.create({
+        departmentName: departmentData.departmentName,
+      })
+        .then(() => {
+          resolve('New department added!');
+        })
+        .catch(() => {
+          reject('Unable to add the new department!');
+        });
+    });
+  },
+
+  updateDepartment: function (departmentData) {
+    for (let field in departmentData) {
+      if (departmentData[field] == '') departmentData[field] = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      Department.update(
+        {
+          departmentName: departmentData.departmentName,
+        },
+        {
+          where: {
+            departmentId: departmentData.departmentId,
+          },
+        }
+      )
+        .then(() => {
+          resolve('Department information has been updated!');
+        })
+        .catch(() => {
+          reject('Unable to update the department information!');
+        });
+    });
+  },
+
+  deleteDepartmentById: function (id) {
+    return new Promise((resolve, reject) => {
+      Department.destroy({
+        where: { departmentId: id },
+      })
+        .then(() => {
+          resolve('Department has been deleted!');
+        })
+        .catch(() => {
+          reject('Unable to delete the department!');
+        });
+    });
+  },
+
+  deleteEmployeeByNum: function (empNum) {
+    return new Promise((resolve, reject) => {
+      Employee.destroy({
+        where: { employeeNum: empNum },
+      })
+        .then(() => {
+          resolve('Employee has been deleted!');
+        })
+        .catch(() => {
+          reject('Unable to delete the employee!');
+        });
+    });
+  },
+
+  //////////////////////////////////////////////////////////////////////////////
+  /////////////// Extra Feature: add, update, and delete a manager ////////////
+  addManager: function (employeeData) {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    for (let field in employeeData) {
+      if (employeeData[field] == '') employeeData[field] = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      Employee.create({
+        firstName: employeeData.firstName,
+        lastName: employeeData.lastName,
+        email: employeeData.email,
+        SSN: employeeData.SSN,
+        addressStreet: employeeData.addressStreet,
+        addressCity: employeeData.addressCity,
+        addressState: employeeData.addressState,
+        addressPostal: employeeData.addressPostal,
+        isManager: true,
+        employeeManagerNum: employeeData.employeeManagerNum,
+        status: employeeData.status,
+        hireDate: employeeData.hireDate,
+        department: employeeData.department,
+      })
+        .then(() => {
+          resolve('New manager added!');
+        })
+        .catch(() => {
+          reject('Unable to add the new manager!');
+        });
+    });
+  },
+
+  updateManager: function (employeeData) {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    for (let field in employeeData) {
+      if (employeeData[field] == '') employeeData[field] = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      Employee.update(
+        {
+          firstName: employeeData.firstName,
+          lastName: employeeData.lastName,
+          email: employeeData.email,
+          SSN: employeeData.SSN,
+          addressStreet: employeeData.addressStreet,
+          addressCity: employeeData.addressCity,
+          addressState: employeeData.addressState,
+          addressPostal: employeeData.addressPostal,
+          isManager: true,
+          employeeManagerNum: employeeData.employeeManagerNum,
+          status: employeeData.status,
+          hireDate: employeeData.hireDate,
+          department: employeeData.department,
+        },
+        {
+          where: {
+            employeeNum: employeeData.employeeNum,
+          },
+        }
+      )
+        .then(() => {
+          resolve('Manager information has been updated!');
+        })
+        .catch(() => {
+          reject('Unable to update the manager information!');
+        });
+    });
+  },
+
+  deleteManagerByNum: function (empNum) {
+    return new Promise((resolve, reject) => {
+      Employee.destroy({
+        where: { employeeNum: empNum },
+      })
+        .then(() => {
+          resolve('Manager has been deleted!');
+        })
+        .catch(() => {
+          reject('Unable to delete the manager!');
+        });
+    });
+  },
+}; //end of module.exports
---
+++ Assignment-6/web322-app/server.js
@@ -0,0 +1,376 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 05
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name: Amirhossein Sabagh
+ *  Student ID: 152956199
+ *  Date: 2021-03-26
+ *
+ *  Online (Heroku) Link: https://quiet-river-67944.herokuapp.com
+ *
+ ********************************************************************************/
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+const multer = require('multer');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const dataService = require('./data-service.js');
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHTTPStart() {
+  console.log('Express http server listening on: ' + HTTP_PORT);
+}
+
+// set up engine for handlebars
+app.engine(
+  '.hbs',
+  exphbs({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+      //fixing the navbar 'active' issue
+      navLink: (url, options) => {
+        return (
+          '<li' +
+          (url == app.locals.activeRoute ? ' class="active" ' : '') +
+          '><a href="' +
+          url +
+          '">' +
+          options.fn(this) +
+          '</a></li>'
+        );
+      },
+      equal: (lvalue, rvalue, options) => {
+        if (arguments.length < 3)
+          throw new Error('Handlebars Helper equal needs 2 parameters');
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
+        }
+      },
+    },
+  })
+);
+app.set('view engine', '.hbs');
+
+//the middleware to support image uploads
+const storage = multer.diskStorage({
+  destination: './public/images/uploaded',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+// tell multer to use the diskStorage function for naming files instead of the default.
+const upload = multer({ storage: storage });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = route == '/' ? '/' : route.replace(/\/$/, '');
+  next();
+});
+
+//for your server to correctly return the "css/site.css" file, the "static" middleware must be used
+// setup the static folder that static resources can load from
+// like images, css files, etc.
+app.use(express.static('public'));
+
+//// setup a 'route' to listen on /home
+app.get('/', (req, res) => {
+  // res.sendFile(path.join(__dirname, '/views/home.html'));
+  res.render('home');
+});
+
+//// setup a 'route' to listen on /about
+app.get('/about', (req, res) => {
+  // res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.render('about');
+});
+
+//// setup a 'route' to listen on /employees/add
+app.get('/employees/add', (req, res) => {
+  // res.sendFile(path.join(__dirname, '/views/addEmployee.html'));
+  dataService
+    .getDepartments()
+    .then((value) => res.render('addEmployee', { departments: value }))
+    .catch(() => res.render('addEmployee', { departments: [] }));
+});
+
+//// setup a 'route' to listen on /images/add
+app.get('/images/add', (req, res) => {
+  // res.sendFile(path.join(__dirname, '/views/addImage.html'));
+  res.render('addImage');
+});
+
+//// setup a 'route' to listen on /employees
+app.get('/employees', (req, res) => {
+  if (req.query.status) {
+    dataService
+      .getEmployeesByStatus(req.query.status)
+      .then((data) => {
+        res.render('employees', { employees: data });
+      })
+      .catch((err) => {
+        res.render('employees', { message: err });
+      });
+  } else if (req.query.department) {
+    dataService
+      .getEmployeesByDepartment(req.query.department)
+      .then((data) => {
+        res.render('employees', { employees: data });
+      })
+      .catch((err) => {
+        res.render('employees', { message: err });
+      });
+  } else if (req.query.manager) {
+    dataService
+      .getEmployeesByManager(req.query.manager)
+      .then((data) => {
+        res.render('employees', { employees: data });
+      })
+      .catch((err) => {
+        res.render('employees', { message: err });
+      });
+  } else {
+    dataService
+      .getAllEmployees()
+      .then((data) => {
+        res.render('employees', { employees: data });
+      })
+      .catch((err) => {
+        res.render('employees', { message: err });
+      });
+  }
+});
+
+app.get('/employee/:empNum', (req, res) => {
+  // initialize an empty object to store the values
+  let viewData = {};
+
+  dataService
+    .getEmployeeByNum(req.params.empNum)
+    .then((data) => {
+      if (data) {
+        viewData.employee = data; //store employee data in the "viewData" object as "employee"
+      } else {
+        viewData.employee = null; // set employee to null if none were returned
+      }
+    })
+    .catch(() => {
+      viewData.employee = null; // set employee to null if there was an error
+    })
+    .then(dataService.getDepartments)
+    .then((data) => {
+      viewData.departments = data; // store department data in the "viewData" object as "departments"
+
+      // loop through viewData.departments and once we have found the departmentId that matches
+      // the employee's "department" value, add a "selected" property to the matching
+      // viewData.departments object
+
+      for (let i = 0; i < viewData.departments.length; i++) {
+        if (
+          viewData.departments[i].departmentId == viewData.employee.department
+        ) {
+          viewData.departments[i].selected = true;
+        }
+      }
+    })
+    .catch(() => {
+      viewData.departments = []; // set departments to empty if there was an error
+    })
+    .then(() => {
+      if (viewData.employee == null) {
+        // if no employee - return an error
+        res.status(404).send('Employee Not Found');
+      } else {
+        res.render('employee', { viewData: viewData }); // render the "employee" view
+      }
+    });
+});
+
+app.get('/employees/delete/:empNum', (req, res) => {
+  dataService
+    .deleteEmployeeByNum(req.params.empNum)
+    .then(() => res.redirect('/employees'))
+    .catch(() => {
+      res.status(500).render('employee', {
+        message: '500: Unable to Remove Employee / Employee not found',
+      });
+    });
+});
+
+app.get('/department/:departmentId', (req, res) => {
+  dataService
+    .getDepartmentById(req.params.departmentId)
+    .then((value) => {
+      res.render('department', { department: value });
+    })
+    .catch((err) => {
+      res
+        .status(404)
+        .render('department', { message: '404: Department Not Found' });
+    });
+});
+
+app.get('/departments/delete/:departmentId', (req, res) => {
+  dataService
+    .deleteDepartmentById(req.params.departmentId)
+    .then(res.redirect('/departments'))
+    .catch((err) => {
+      res.status(500).render('department', {
+        message: 'Unable to Remove Department / Department not found)',
+      });
+    });
+});
+
+//// setup a 'route' to listen on /managers
+app.get('/managers', (req, res) => {
+  dataService
+    .getManagers()
+    .then((data) => {
+      res.render('managers', { managers: data });
+    })
+    .catch((err) => {
+      res.render('managers', { message: err });
+    });
+});
+
+//// setup a 'route' to listen on /departments
+app.get('/departments', (req, res) => {
+  dataService
+    .getDepartments()
+    .then((data) => {
+      res.render('departments', { departments: data });
+    })
+    .catch((err) => {
+      res.render('departments', { message: err });
+    });
+});
+
+app.get('/departments/add', function (req, res) {
+  res.render('addDepartment');
+});
+
+app.get('/images', (req, res) => {
+  fs.readdir('./public/images/uploaded', (err, items) => {
+    res.render('images', { image: items });
+  });
+});
+
+app.post('/images/add', upload.single('imageFile'), (req, res) => {
+  res.redirect('/images');
+});
+
+app.post('/employees/add', (req, res) => {
+  dataService
+    .addEmployee(req.body)
+    .then(res.redirect('/employees'))
+    .catch((err) => {
+      res
+        .status(500)
+        .render('employee', { message: '500: Unable to add the employee' });
+    });
+});
+
+app.post('/employee/update', (req, res) => {
+  dataService
+    .updateEmployee(req.body)
+    .then(res.redirect('/employees'))
+    .catch((err) => {
+      res
+        .status(500)
+        .render('employee', { message: '500: Unable to update the employee' });
+    });
+});
+
+app.post('/departments/add', (req, res) => {
+  dataService
+    .addDepartment(req.body)
+    .then(res.redirect('/departments'))
+    .catch((err) => {
+      res.status(500).render('departments', {
+        message: '500: Unable to add the department',
+      });
+    });
+});
+
+app.post('/department/update', (req, res) => {
+  dataService
+    .updateDepartment(req.body)
+    .then(res.redirect('/departments'))
+    .catch((err) => {
+      res.status(500).render('departments', {
+        message: '500: Unable to update the department',
+      });
+    });
+});
+
+//////////////////////////////////////////////////////////////////////////////
+/////// Extra Feature: Middleware to add, update, and delete a manager ///////
+app.get('/managers/add', function (req, res) {
+  res.render('addManager');
+});
+
+app.post('/managers/add', (req, res) => {
+  dataService
+    .addManager(req.body)
+    .then(res.redirect('/managers'))
+    .catch((err) => {
+      res.status(500).render('managers', {
+        message: '500: Unable to add the manager',
+      });
+    });
+});
+
+app.post('/manager/update', (req, res) => {
+  dataService
+    .updateManager(req.body)
+    .then(res.redirect('/managers'))
+    .catch((err) => {
+      res.status(500).render('managers', {
+        message: '500: Unable to update the manager',
+      });
+    });
+});
+
+app.get('/managers/delete/:empNum', (req, res) => {
+  dataService
+    .deleteManagerByNum(req.params.empNum)
+    .then(() => res.redirect('/managers'))
+    .catch(() => {
+      res.status(500).render('managers', {
+        message: '500: Unable to Remove Manager / Manager not found',
+      });
+    });
+});
+//////////////////////////////////////////////////////////////////////////////
+
+//// setup a 'route' for no matching route
+//////// Traditional way:
+// app.use(function (req, res) {
+//   res.status(404).send('Page Not Found');
+// });
+//////// Modern way:
+app.use(function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/404.html'));
+});
+
+//// setup http server to listen on HTTP_PORT
+dataService
+  .initialize()
+  .then(function () {
+    app.listen(HTTP_PORT, onHTTPStart);
+  })
+  .catch(function (err) {
+    console.log('Failed to start!' + err);
+  });
